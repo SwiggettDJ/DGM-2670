7d2fcbc2f2342b6783eda2ae661be461652fc5bd
DJ <swiggettdj@gmail.com>
2021-09-15 00:16:48 -0600

    Got caught up with scriptable objects

---
+++ New Unity Project/Assets/FloatData.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+    public float value;
+
+    public void AddToValue(float num)
+    {
+        value += num;
+    }
+
+    public void ResetValue(float num)
+    {
+        value = num;
+    }
+
+    public void AddToValueZero(float num)
+    {
+        if (value <= 0)
+        {
+            value = 0;
+        }
+        else
+        {
+            value += num;
+        }
+    }
+}
--- New Unity Project/Assets/RigidBodyBehaviour.cs
+++ New Unity Project/Assets/RigidBodyBehaviour.cs
@@ -8,7 +8,7 @@ public class RigidBodyBehaviour : MonoBehaviour
     public float force;
     private Rigidbody rb;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.AddExplosionForce(force, Vector3.right, 30f);
